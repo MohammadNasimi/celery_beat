da0cb4e2d1dbc37d724228b4f886ab5b2ed7bf5d
mohammadnasimi <mohammadnasimi6803@gmail.com>
2022-12-22 13:24:23 +0330

    model news

--- rss/models.py
+++ rss/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class news (models.Model):
+    title = models.TextField(max_length=200)
+    updated = models.TextField(max_length=200)
+    summary = models.TextField(max_length=200)
+    link = models.URLField(max_length=128,db_index=True,unique=True,blank=True)
