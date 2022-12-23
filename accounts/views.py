53c806d40adc270ee2472ed94a51c978d445bcb2
mohammadnasimi <mohammadnasimi6803@gmail.com>
2022-12-23 16:23:47 +0330

    add register view

--- accounts/views.py
+++ accounts/views.py
@@ -34,3 +34,32 @@ class LoginView(generics.GenericAPIView):
 
         else:
             return Response({'user':'wrong username or password'}, status=status.HTTP_200_OK)
+        
+        
+##############################################
+############register driver ##################
+class RegisterDriverView(generics.APIView):
+    def post(self, request, *args, **kwargs):
+        email= request.data.get("email" ,"")
+        first_name= request.data.get("first_name" ,"")
+        last_name= request.data.get("last_name" ,"")
+        # create-user
+        def create_user():
+            user =User.objects.create_user(phone=request.data.get('phone'),password=request.data.get('password'),
+                email=email,first_name=first_name,
+                last_name=last_name)
+            return user
+        
+        
+        if request.data.get('phone') == None or request.data.get('password') == None \
+                            or request.data.get('password2') == None:
+                return Response({"detail": "اطلاعات ارسالی کامل نیست."} , status=status.HTTP_400_BAD_REQUEST)
+        try:
+            if request.data.get('password')==request.data.get('password2'):
+                user =create_user()
+            else:
+                return Response({'detail:':'password must to mutch'},
+                                status=status.HTTP_400_BAD_REQUEST)
+        except:
+                return Response({"detail"  : "username exist"} , status=status.HTTP_400_BAD_REQUEST)
+        return Response(request.data, status=status.HTTP_201_CREATED)
