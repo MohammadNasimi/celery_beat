8fe93f902aa97a82e4c9569a98c44c31de9c0df8
mohammadnasimi <mohammadnasimi6803@gmail.com>
2022-12-23 17:32:32 +0330

    add get query set def

--- rss/views.py
+++ rss/views.py
@@ -1,11 +1,21 @@
 from bs4 import BeautifulSoup
 import requests
-from rest_framework.views import APIView
+# import rest framework
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.generics import ListAPIView
 from rest_framework.response import Response
 from rest_framework import status
+
+# rss app 
 from rss.models import news
+# import celery 
 from celery import shared_task
-@shared_task
+from task.celery import app
+
+from rss.serializers import newsSerializer
+
+
+@app.task
 def create_news(entries):
         for entry in range(5):
             title = entries[entry].title.text
@@ -14,12 +24,25 @@ def create_news(entries):
             link = entries[entry].link['href']
             news.objects.create(title= title , updated = updated,summary = summary,link = link)
 
-class rsslistView(APIView):
+class rsslistView(ListAPIView):
+    serializer_class = newsSerializer
+    permission_classes =[IsAuthenticated]
+     
+    def get_queryset(self):
+        queryset =news.objects.all()
+        return queryset
+
   
     def get(self, request, format=None):
         url = requests.get('https://realpython.com/atom.xml')
         soup = BeautifulSoup(url.content ,'xml')
         entries = soup.find_all('entry')
         create_news.delay(entries)
-        data = {'m':12}
-        return Response(data,status=status.HTTP_400_BAD_REQUEST)
+        queryset = self.filter_queryset(self.get_queryset())
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data,status= status.HTTP_200_OK)
