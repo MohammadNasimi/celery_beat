8e7c246fae056ac7f661bc9ece9929f2939c4f3f
mohammadnasimi <mohammadnasimi6803@gmail.com>
2022-12-23 17:05:26 +0330

    change field update

--- rss/models.py
+++ rss/models.py
@@ -4,6 +4,6 @@ from django.db import models
 
 class news (models.Model):
     title = models.TextField(max_length=200)
-    updated = models.TextField(max_length=200)
+    updated = models.DateTimeField()
     summary = models.TextField(max_length=200)
     link = models.URLField(max_length=128,db_index=True,unique=True,blank=True)
