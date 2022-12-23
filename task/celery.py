130ee574653f6b643424b1cb86c59b6a1730f591
mohammadnasimi <mohammadnasimi6803@gmail.com>
2022-12-23 17:34:48 +0330

    change name to task

--- task/celery.py
+++ task/celery.py
@@ -1,12 +1,14 @@
 from celery import Celery
-
-app = Celery('rss')
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE','task.settings')
+app = Celery('task')
 
 app.config_from_object('django.conf:settings',namespace = 'CELERY')
 
+app.autodiscover_tasks()
 app.conf.beat_schedule ={
-    'rss_task_every_5_min':{
-        'rss' : 'rss.views.create_news',
+    'task_task_every_5_min':{
+        'task' : 'rss.views.create_news',
         'schedule': 60*5
         
     }
